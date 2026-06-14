a300654a713cdca72b329e950c451b9ed0f19307
LauraEcue <lauravalentinacastillo23@gmail.com>
2026-06-14 16:53:23 -0500

    feat

---
+++ repaso.py
@@ -0,0 +1,10 @@
+class coche:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+
+    def describir(self):
+        return f"coche {self.marca} de color {self. modelo}"
+
+    
+
