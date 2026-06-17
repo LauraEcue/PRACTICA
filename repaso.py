0e53ac7eb35bc607ce9f01b05d831c3c13b48a24
LauraEcue <lauravalentinacastillo23@gmail.com>
2026-06-16 19:14:53 -0500

    feat

--- repaso.py
+++ repaso.py
@@ -1,10 +1,18 @@
-class coche:
-    def __init__(self, marca, modelo):
+class Ropa:
+    def __init__(self, marca, talla):
         self.marca = marca
-        self.modelo = modelo
+        self.talla = talla
 
     def describir(self):
-        return f"coche {self.marca} de color {self. modelo}"
+        return f"yo tengo una camisa de {self.marca}, mi talla es {self. talla}."
+
+
+#probar
+ropita = Ropa("clemont", "S")
+print(ropita.describir())
+
+
+    
 
     
 
