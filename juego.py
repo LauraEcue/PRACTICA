0845b9d0d452fa64a15b3369894c3f5c1b6a7f13
LauraEcue <lauravalentinacastillo23@gmail.com>
2026-06-30 21:24:46 -0500

    feat

--- juego.py
+++ juego.py
@@ -10,5 +10,37 @@ class Personaje:
 
         print(f"{self.nombre} recibio {self.cantidad} de daño. ")
         (f"vida restante:{self.__vida}")
+
+
+        if self.__vida <= 0:
+            self.__vida = 0
+            print (f"{self.nombre} caido en combate")
+
+        else:
+            print (f"{self.nombre} esquivó")
+
+
+    def Atacar(self):
+        print(f"{self.nombre} realiza ataque")
+
+    def Esta_vivo(self):
+        return self.__vida > 0
+
+    def Get__vida(self):
+        return self.__vida
+
+
+#otra clase con herencia
+
+class Guerro(Personaje):
+    def __init__(self, nombre, vida, nivel, fuerza):
+        super().__init__ (self, nombre, vida, nivel)
+        self.fuerza = fuerza
+
+    def Atacar(self):
+        daño = self.nivel + self.fuerza
+        return daño
+
+
         
 
